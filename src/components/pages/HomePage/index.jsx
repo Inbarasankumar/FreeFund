9bad3d71f2cddcd0b96447814678778cbd9d24d1
tejas2195 <tejas@nineleaps.com>
2020-05-15 23:57:26 +0530

    Login modal

--- src/components/pages/HomePage/index.jsx
+++ src/components/pages/HomePage/index.jsx
@@ -1,11 +1,32 @@
 import React, {Component} from 'react';
-import {Row, Col, Carousel} from 'antd';
+import { Modal, Row, Col, Carousel} from 'antd';
 import {AppstoreFilled} from '@ant-design/icons';
 import Images from '../../assets/images';
 import './homePage.scss';
 import DetailsCard from '../../common/detailsCard';
+import LoginTab from '../LoginPage';
 
 class HomePage extends Component {
+    constructor(props) {
+        super(props);
+            this.state = {
+                isVisible: false
+            }
+    }
+
+    onButtonClick = () => {
+        console.log('clicked!!');
+        this.setState({
+            isVisible: true
+        })
+    }
+
+    handleCancel = () => {
+        this.setState({
+          isVisible: false,
+        });
+      };
+
     render() {
         return (
             <>
@@ -126,7 +147,7 @@ class HomePage extends Component {
                                     Lorem Ipsum has been the industry’s
                                     standard dummy text ever since the 1500s
                                 </p>
-                                <button className="select-button">Start Earning</button>
+                                <button onClick={this.onButtonClick} className="select-button">Start Earning</button>
                             </Col>
                             <Col span={6}>
                                 <h1 className="user-type-heading">Creators</h1>
@@ -137,7 +158,7 @@ class HomePage extends Component {
                                     Lorem Ipsum has been the industry’s
                                     standard dummy text ever since the 1500s
                                 </p>
-                                <button className="select-button">Post a project</button>
+                                <button onClick={this.onButtonClick} className="select-button">Post a project</button>
                             </Col>
                             <Col span={6}>
                                 <h1 className="user-type-heading">Backers</h1>
@@ -148,7 +169,7 @@ class HomePage extends Component {
                                     Lorem Ipsum has been the industry’s
                                     standard dummy text ever since the 1500s
                                 </p>
-                                <button className="select-button">Start Investing</button>
+                                <button onClick={this.onButtonClick} className="select-button">Start Investing</button>
                             </Col>
                         </Row>
                     </Col>
@@ -165,6 +186,21 @@ class HomePage extends Component {
                         </div>
                     </Col>
                 </Row>
+                (this.state.isVisible? (
+                    <div className="modal-display">
+                    <Modal
+                        closable={false}
+                        className="login-modal"
+                        centered={true}
+                        visible={this.state.isVisible}
+                        footer={null}
+                        onCancel={this.handleCancel}
+                    >
+                        <LoginTab />
+                    </Modal>
+                  </div>
+                ): null
+            )                
             </>
         );
     }
--- src/components/pages/LoginPage/index.jsx
+++ src/components/pages/LoginPage/index.jsx
@@ -1,75 +1,49 @@
 import React from 'react';
-import {Paper, withStyles, Grid, TextField, Button} from '@material-ui/core';
-import { Facebook} from '@material-ui/icons'
-import './loginPage.scss'
-import Images from "../../assets/images";
-import {Col} from "antd";
-
-const styles = theme => ({
-    margin: {
-        margin: theme.spacing.unit * 2,
-    },
-    padding: {
-        padding: theme.spacing.unit
-    }
-});
+import { Row, Col } from 'antd';
+import { FacebookOutlined, GoogleOutlined } from '@ant-design/icons';
+import Images from '../../assets/images';
+import './loginPage.scss';
+import { Link } from 'react-router-dom';
 
 class LoginTab extends React.Component {
-    render() {
-        const {classes} = this.props;
-        return (
-            <Paper className="login">
-                <div className={classes.margin}>
-                    <Grid container justify="center" style={{marginTop: '10px', marginBottom: '20px'}}>
-                        <Col span={3}>
-                            <img src={Images.freefundLogo} alt="freefundLogo"/>
-                        </Col>
-                    </Grid>
-                    <Grid container justify="center" style={{marginTop: '10px', marginBottom: '20px'}}>
-                        <Col span={3}>
-                            <h3 className="content-2-heading">Join now</h3>
-                        </Col>
-                    </Grid>
-                    <Grid className="facebook" container  style={{textTransform: "none", backgroundColor: ' #3c5a9a',
-                        width: '250px', marginTop: '20px', alignContent: 'center', justify: "center"}}>
-                        <Col span={3} justify="center">
-                            <img src={Images.facebook} alt="facebook"/>
-                        </Col>
-                        <Button color="white" style={{textTransform: "none"}}>Continue with facebook</Button>
-                    </Grid>
-                    <Grid className="google" container justify="center" style={{textTransform: "none", backgroundColor: ' #ffffff',
-                        width: '250px', marginTop: '20px'}}>
-                        <Col span={3} justify="center">
-                            <img src={Images.google} alt="google"/>
-                        </Col>
-                        <Button color="grey" style={{textTransform: "none"}}>Continue with Google</Button>
-                    </Grid>
-                    <Grid classname="continue" container justify="center" style={{marginTop: '10px'}}>
-                        <Col span={3}>
-                            <h3 className="content-2-heading">OR</h3>
-                        </Col>
-                    </Grid>
 
-                    <Grid container justify="center">
-                        <Grid item >
-                            <TextField id="username" label="Enter your email" type="email" autoFocus required/>
-                        </Grid>
-                    </Grid>
-                    <Grid classname="continue" container justify="center" style={{marginTop: '10px'}}>
-                        <Button variant="outlined" color=" #28bc78"
-                                style={{textTransform: "none", backgroundColor: ' #28bc78',
-                                    width: '200px', marginTop: '20px'}}>Continue</Button>
-                    </Grid>
-                    <Grid classname="continue" container justify="center" style={{marginTop: '10px'}}>
-                        <Col span={3}>
-                            <h3 style={{width: '300px', alignItems: 'flex-start'}}>By joining you agree to our Terms and Conditions
-                                and Privacy Policy.</h3>
-                        </Col>
-                    </Grid>
-                </div>
-            </Paper>
-        );
+    navigateToBrowsePage = () => {
+        this.props.history.push('/browseprojects');
     }
+
+  render() {
+    return (
+      <div>
+            <Row justify="center">
+                <Col span={12} offset={6}>
+                    <img src={Images.freefundLogo} alt="freefundLogo"/>
+                    <p className="join-now">Join Now</p>
+                </Col>
+            </Row>
+            <Row justify="center">
+                <Col span={16} offset={3}>
+                    <button className="sign-in">
+                        <FacebookOutlined className="button-logo" />
+                        <span className="button-name">
+                            Continue with Facebook
+                        </span>
+                    </button>
+                    <button onClick={this.navigateToBrowsePage} className="sign-in">
+                        <GoogleOutlined className="button-logo" />
+                        <span className="button-name">
+                            Continue with Google
+                        </span>
+                    </button>
+                </Col>
+            </Row>
+            <Row justify="center">
+                <Col span={16} offset={3}>
+                    <p className="terms-condition">By joining you agree to our <Link className="link">Terms and Conditions and Privacy Policy.</Link></p>
+                </Col>
+            </Row>
+      </div>
+    );
+  }
 }
 
-export default withStyles(styles)(LoginTab);
+export default LoginTab;
