653c29fcf77bdcdad14a8779d7814bb62681dbf0
Abhilash74 <abhilash.c@nineleaps.com>
2020-05-15 10:11:10 +0530

    added images

---
+++ src/components/pages/BrowseProject/index.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Header from '../../common/Header';
+import { Row, Col } from 'antd';
+import { AppstoreFilled } from '@ant-design/icons';
+import Images from '../../assets/images';
+import './browseProject.scss';
+import HomePage from "../HomePage";
+
+class BrowseProject extends Component {
+    render() {
+        return (
+            <>
+
+            </>
+        )
+    }
+}
+
+export default BrowseProject;
