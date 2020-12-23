6851f9a56c8bd780049878bf389bb73afcaee656
Inbarasankumar <inbarasanraja@gmail.com>
2020-12-23 23:33:21 +0530

    Updates

---
+++ precache-manifest.3ec2584e7be0757af85955b7d4cb252c.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eb792fbd0691e35456f93de13bc0dbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9a4f1c86c14bfe55bc1",
+    "url": "/static/css/2.d7262850.chunk.css"
+  },
+  {
+    "revision": "e116b8d28e2d114b8c94",
+    "url": "/static/css/main.43f086db.chunk.css"
+  },
+  {
+    "revision": "d9a4f1c86c14bfe55bc1",
+    "url": "/static/js/2.f2da3d4a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f2da3d4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e116b8d28e2d114b8c94",
+    "url": "/static/js/main.a064ef7c.chunk.js"
+  },
+  {
+    "revision": "7ed19acb04e6ed53b144",
+    "url": "/static/js/runtime-main.72a55ad9.js"
+  },
+  {
+    "revision": "b98524023a28557a2905e56067cc56c9",
+    "url": "/static/media/Interface.b9852402.png"
+  },
+  {
+    "revision": "69deada17154996dc60b3e06ca86f27b",
+    "url": "/static/media/Under Construction.69deada1.png"
+  },
+  {
+    "revision": "b53e0e2967b3cca7c76a95aaa6d47250",
+    "url": "/static/media/app design.b53e0e29.svg"
+  },
+  {
+    "revision": "4c7792bb70416e557e53764567ebf045",
+    "url": "/static/media/app-development.4c7792bb.png"
+  },
+  {
+    "revision": "b8580f67680d84baf10d1ae01ffa914f",
+    "url": "/static/media/computer.b8580f67.png"
+  },
+  {
+    "revision": "056e52c38437d4be34267736559a05bc",
+    "url": "/static/media/freefundLogo.056e52c3.svg"
+  },
+  {
+    "revision": "d26d80daa3dda3ef8fcf84c4634baf67",
+    "url": "/static/media/graphic.d26d80da.png"
+  },
+  {
+    "revision": "69570fe41dffdcc9d3d9990c429deb12",
+    "url": "/static/media/group.69570fe4.png"
+  },
+  {
+    "revision": "1ccca66f02e8998e2b3354244933b377",
+    "url": "/static/media/homePageLaptop.1ccca66f.jpg"
+  },
+  {
+    "revision": "8bc572ff71d49c07066b80ba667ea0f7",
+    "url": "/static/media/laptop-code.8bc572ff.jpg"
+  },
+  {
+    "revision": "fad49baeb74808b09d275df2bcc0a3dd",
+    "url": "/static/media/mobile-map.fad49bae.png"
+  },
+  {
+    "revision": "aa1f481c2184c6f0de2f05c435ffcb72",
+    "url": "/static/media/music-system.aa1f481c.png"
+  },
+  {
+    "revision": "f09e5e94ddceddfae610562dbe1710a6",
+    "url": "/static/media/music.f09e5e94.png"
+  },
+  {
+    "revision": "5b681c69d698c06671e0997e16959eec",
+    "url": "/static/media/the-gifted-amy-acker-02.5b681c69.png"
+  }
+]);
